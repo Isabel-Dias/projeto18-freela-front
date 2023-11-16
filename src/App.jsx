61f74de4b49c4918a26e3da865dc69f7c30cb70a
Isabel-Dias <isabel.c.andrade.dias@gmail.com>
2023-11-16 20:52:29 -0300

    delete: unused user context

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-import { useState } from "react";
 import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
 import AddServicePage from "./pages/AddServicePage";
 import ServicesPage from "./pages/ServicesPage";
-import UserContext from "./contexts/UserContext";
 import ServiceDetailsPage from "./pages/ServiceDetailsPage";
 
 function App() {
-  const [userData, setUserData] = useState("");
-
   return (
     <BrowserRouter>
-      <UserContext.Provider value={{ userData, setUserData }}>
-        <Routes>
-          <Route path="/" element={<SignInPage />} />
-          <Route path="/signUp" element={<SignUpPage />} />
-          <Route path="allServices" element={<ServicesPage />} />
-          <Route path="/addService" element={<AddServicePage />} />
-          <Route path="/services/:id" element={<ServiceDetailsPage />} />
-        </Routes>
-      </UserContext.Provider>
+      <Routes>
+        <Route path="/" element={<SignInPage />} />
+        <Route path="/signUp" element={<SignUpPage />} />
+        <Route path="allServices" element={<ServicesPage />} />
+        <Route path="/addService" element={<AddServicePage />} />
+        <Route path="/services/:id" element={<ServiceDetailsPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
